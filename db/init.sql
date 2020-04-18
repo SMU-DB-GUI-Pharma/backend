5f372a617a0166f76cdc7516c644d05be48de2b9
fidelianawar <fidelia.m589@gmail.com>
2020-04-17 21:05:05 -0500

    updated init.sql

--- db/init.sql
+++ db/init.sql
@@ -6,59 +6,61 @@ CREATE TABLE `db`.`test_table` (
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
 );
 
---create user table
-CREATE TABLE 'User' (
-    'InsuranceID' INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY ('InsuranceID'),
-    'Username' VARCHAR(45),
-    'Password' VARCHAR(45),
-    'PinCode' INT,
-    'PharmacyID' INT,
-    'TotalCostPrescriptions' DECIMAL, 
-    'MonthlyCost' DECIMAL, 
-);
-
 --create pharmacy table
 CREATE TABLE 'Pharmacy' (
     'PharmacyName' VARCHAR(45),
     
 );
 
---create prescription brand table
-CREATE TABLE 'PrescriptionBrand' (
-    'BrandID' INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY ('BrandID'),
-    'BrandName' VARCHAR(45),
-    'Description' VARCHAR(256)
+--create user table
+CREATE TABLE User(
+    InsuranceID INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (InsuranceID),
+    Username VARCHAR(45),
+    Password VARCHAR(45),
+    PinCode INT,
+    PharmacyID INT,
+    TotalCostPrescriptions DECIMAL, 
+    MonthlyCost DECIMAL
 );
 
---create prescriptions table
-CREATE TABLE 'Prescripton' (
-    'Name' VARCHAR(45),
-    'BrandID' INT,
-    'DateOfIssue' DATETIME,
-    'DateOfExpiration' DATETIME,
-    'RefillDate' DATETIME,
-    'RefillCount' INT,
-    'PauseDate' DATETIME,
-    'isRefillable' BOOLEAN,
-    'isReccurring' BOOLEAN,
-    'Description' VARCHAR (256),
-    'Price' DECIMAL,
+--create user prescription brand table
+CREATE TABLE PrescriptionBrand (
+    BrandID INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (BrandID),
+    BrandName VARCHAR(45),
+    Description VARCHAR(256)
 );
+
+--create prescription table
+CREATE TABLE Prescripton (
+    Name VARCHAR(45),
+    BrandID INT,
+    DateOfIssue DATETIME,
+    DateOfExpiration DATETIME,
+    RefillDate DATETIME,
+    RefillCount INT,
+    PauseDate DATETIME,
+    isRefillable BOOLEAN,
+    isReccurring BOOLEAN,
+    Description VARCHAR (256),
+    Price DECIMAL
+);
+
 --create insurance table
-CREATE TABLE 'Insurance' (
-    'InsuranceID' INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY ('InsuranceID'),
-    'Company' VARCHAR(45),
-    'AddressLine1' VARCHAR(45),
-    'AddressLine2' VARCHAR(45),
-    'City' VARCHAR(45),
-    'State' VARCHAR(45),
-    'PostalCode' INT,
-    'PhoneNumber' INT,
-    'Email' VARCHAR(45)
+CREATE TABLE Insurance (
+    InsuranceID INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (InsuranceID),
+    Company VARCHAR(45),
+    AddressLine1 VARCHAR(45),
+    AddressLine2 VARCHAR(45),
+    City VARCHAR(45),
+    State VARCHAR(45),
+    PostalCode INT,
+    PhoneNumber INT,
+    Email VARCHAR(45)
 );
+
 
 -- insert sample entry
 INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
